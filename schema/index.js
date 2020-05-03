9d940e7507b84f8f4c1efd5a5b57c4f29efb0b5d
luckyaxl <luckyaxl3@gmail.com>
2020-05-03 19:12:27 +0700

    Mutation Delete

--- schema/index.js
+++ schema/index.js
@@ -106,6 +106,15 @@ const mutation = new GraphQLObjectType({
           .post(`http://localhost:3000/categories`, args)
           .then(res => res.data);
       }
+    },
+    deleteCategory: {
+      type: CategoryType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios
+          .delete(`http://localhost:3000/categories/${args.id}`)
+          .then(res => res.data);
+      }
     }
   }
 });
